fd4396a7b3deafd54bc83b03f86fb8bdcaced1ad
donghyeon-100 <goat.ohgiraffers@gmail.com>
2025-04-03 11:31:13 +0900

    router 과제

---
+++ 과제/20250328/심화/router-test/src/pages/About.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="card card-body">
+    <h2>About</h2>
+    <p>요청경로 : {{ $route.fullPath }}</p>
+  </div>
+</template>
+<script>
+export default {
+  name: "About",
+  created() {
+    console.log(this.$route);
+  },
+};
+</script>
+
+<!-- 출력결과 다름, 실제 사용 route 정보만 포함된 객체 출력 -->
+<!-- <template>
+  <div class="card card-body">
+    <h2>About</h2>
+    <p>요청경로 : {{ route.fullPath }}</p>
+  </div>
+</template>
+
+<script>
+import { onMounted } from "vue";
+import { useRoute } from "vue-router";
+
+export default {
+  name: "About",
+  setup() {
+    const route = useRoute();
+
+    onMounted(() => {
+      console.log(route);
+    });
+
+    return { route };
+  },
+};
+</script> -->
---
+++ 과제/20250328/심화/router-test/src/router/index.js
@@ -0,0 +1,36 @@
+import { createRouter, createWebHistory } from "vue-router"; // 이 줄 추가
+
+import Home from "@/pages/Home.vue";
+import About from "@/pages/About.vue";
+import Members from "@/pages/Members.vue";
+import Videos from "@/pages/Videos.vue";
+import MemberInfo from "@/pages/MemberInfo.vue";
+import NotFound from "@/pages/NotFound.vue";
+
+const membersIdGuard = (to, from) => {
+  // members/:id 경로는 반드시 이전 경로가
+  // /members, /members:id 인 경우만 내비게이션 허용함
+  console.log(`이름은 : ${from.name}`);
+
+  if (from.name !== "members" && from.name !== "members/id") {
+    return false;
+  }
+};
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: "/", component: Home },
+    { path: "/about", component: About },
+    { path: "/members", name: "members", component: Members }, // name 속성 추가
+    {
+      path: "/members/:id",
+      name: "members/id",
+      component: MemberInfo,
+      beforeEnter: membersIdGuard,
+    },
+    { path: "/videos", component: Videos },
+    { path: "/:paths(.*)*", name: "NotFound", component: NotFound },
+  ],
+});
+export default router;
